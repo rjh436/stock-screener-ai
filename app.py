2874ed86239df0d29bfe3087707b17fd858799ec
rjh436 <rjh436@gmail.com>
2025-12-11 11:33:33 -0500

    Updates

--- app.py
+++ app.py
@@ -206,7 +206,7 @@ elif mode == "Backtest":
             {"col": "bb_width", "op": ">", "val": 0.17}
         ],
         "exit_rules": [],
-        "stop_loss_atr": 4.4,
+        "stop_loss_atr": 3.5,
         "time_stop": 71
     }
     
